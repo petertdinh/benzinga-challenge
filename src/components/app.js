87bb3b4c8a41efbaf5152740b2703c6ade190a32
Peter <peter.dinh@gmail.com>
2016-09-17 23:11:14 -0700

    [delete] unnecessary imports in app

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import numeral from 'numeral';
-import { buyStock, sellStock } from '../actions/index';
 import SearchBar from './search_bar';
 import Portfolio from './portfolio';
 import CurrentStock from './current_stock';
-import _ from 'lodash';
 
 
 export default class App extends Component {
